b511e79af9c6adaeea794e4f51977de6d3067cb3
Koray Oksay <koray.oksay@gmail.com>
2023-07-15 13:47:11 +0300

    add kyverno cli to the environment

--- kyverno/intro/assets/init.sh
+++ kyverno/intro/assets/init.sh
@@ -15,5 +15,11 @@ apt-get update
 apt-get install -y bat
 kubectl taint node controlplane node-role.kubernetes.io/control-plane:NoSchedule-
 
+# install kyverno cli
+CLI_VERSION=v1.10.1
+curl -LO https://github.com/kyverno/kyverno/releases/download/${CLI_VERSION}/kyverno-cli_${CLI_VERSION}_linux_x86_64.tar.gz
+tar -xvf kyverno-cli_${CLI_VERSION}_linux_x86_64.tar.gz
+cp kyverno /usr/local/bin/
+
 # mark init finished
 touch /ks/.initfinished
